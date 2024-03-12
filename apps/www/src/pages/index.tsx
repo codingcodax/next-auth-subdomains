53c036cf22ce6653577bba5d08c658dffdaf4feb
Alexis Guzman <codingcodax@gmail.com>
2024-03-11 18:52:00 -0600

    feat(www): make req to app

--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -1,31 +1,53 @@
+import { useEffect, useState } from "react";
 import type { NextPage } from "next";
-import { signIn, signOut } from "next-auth/react";
-
-import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const { data: session } = api.auth.getSession.useQuery();
-  const { data: secretMessage } = api.auth.getSecretMessage.useQuery(
-    undefined,
-    {
-      enabled: !!session?.user,
-    },
-  );
+  const [session, setSession] = useState<{
+    user?: { name: string; email: string; image: string };
+  }>();
+
+  useEffect(() => {
+    const getSession = async () => {
+      const res = await fetch("http://localhost:3000/api/auth/session", {
+        credentials: "include",
+      });
+      console.log("🚀 ~ res:", res);
+      const sess = (await res.json()) as {
+        user?: { name: string; email: string; image: string };
+      };
+      console.log("🚀 ~ sess:", sess);
+      if (sess.user) setSession(sess);
+    };
+
+    async () => await getSession();
+  }, []);
 
   return (
-    <div>
-      <p>home page</p>
-
-      {session?.user && (
-        <p>
-          {session && <span>Logged in as {session?.user?.name}</span>}
-          {secretMessage && <span> - {secretMessage}</span>}
-        </p>
+    <>
+      {session?.user ? (
+        <>
+          <div>
+            <p>{session.user.name}</p>
+            <p>{session.user.email}</p>
+          </div>
+          <a
+            href={`http://localhost:3000/api/auth/signout?callbackUrl=${encodeURIComponent(
+              "http://localhost:3001",
+            )}`}
+          >
+            Sign Out
+          </a>
+        </>
+      ) : (
+        <a
+          href={`http://localhost:3000/api/auth/signin?callbackUrl=${encodeURIComponent(
+            "http://localhost:3001",
+          )}`}
+        >
+          Sign In
+        </a>
       )}
-      <button onClick={session ? () => void signOut() : () => void signIn()}>
-        {session ? "Sign out" : "Sign in"}
-      </button>
-    </div>
+    </>
   );
 };
 
