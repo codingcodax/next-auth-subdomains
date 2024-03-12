607a44ee4148c43bbaf4b6093119b29a251f1c66
Alexis Guzman <codingcodax@gmail.com>
2024-03-12 08:37:19 -0600

    feat(nextconfig): add ehaders to enable cors on app

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,4 +1,5 @@
 // Importing env files here to validate on build
+
 import "./src/env.mjs";
 import "@acme/auth/env.mjs";
 
@@ -10,6 +11,63 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  // eslint-disable-next-line @typescript-eslint/require-await
+  headers: async () => {
+    // To help with local development...
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/auth/:path*",
+          has: [{ type: "header", key: "Origin", value: "(?<origin>.*)" }],
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: ":origin" },
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET, OPTIONS, PATCH, DELETE, POST, PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
+    }
+
+    // In the other environments...
+    return [
+      // https://vercel.com/support/articles/how-to-enable-cors#enabling-cors-in-a-next.js-app
+      // https://nextjs.org/docs/api-reference/next.config.js/headers#header-cookie-and-query-matching
+      {
+        // matching all auth API routes
+        source: "/api/auth/:path*",
+        // if the origin has '.my-domain.com'...
+        has: [
+          {
+            type: "header",
+            key: "Origin",
+            value: "(?<origin>^https:\\/\\/.*\\.codingcodax\\.dev$)",
+          },
+        ],
+        // these headers will be applied
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: ":origin" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, OPTIONS, PATCH, DELETE, POST, PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default config;
