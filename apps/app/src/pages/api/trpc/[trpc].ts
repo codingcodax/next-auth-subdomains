3a845a10f960b772135de0c29531e5e038f68124
Alexis Guzman <codingcodax@gmail.com>
2024-03-12 09:10:01 -0600

    feat(trpc): enable cors for app

--- apps/app/src/pages/api/trpc/[trpc].ts
+++ apps/app/src/pages/api/trpc/[trpc].ts
@@ -1,23 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 import { createNextApiHandler } from "@trpc/server/adapters/next";
+import cors from "cors";
 
 import { appRouter, createTRPCContext } from "@acme/api";
 
 // export API handler
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-});
+// export default createNextApiHandler({
+//   router: appRouter,
+//   createContext: createTRPCContext,
+// });
 
 // If you need to enable cors, you can do so like this:
-// const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-//   // Enable cors
-//   await cors(req, res);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Enable cors
+  await cors(req, res);
 
-//   // Let the tRPC handler do its magic
-//   return createNextApiHandler({
-//     router: appRouter,
-//     createContext,
-//   })(req, res);
-// };
+  // Let the tRPC handler do its magic
+  return createNextApiHandler({
+    router: appRouter,
+    createContext: createTRPCContext,
+  })(req, res);
+};
 
-// export default handler;
+export default handler;
